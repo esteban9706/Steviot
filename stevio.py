fb1df807cc42f27d675b04d7d0203dcdc1dce0e5
Esteban <corlo.esteban@gmail.com>
2020-02-03 00:56:33 -0600

    Added ExecuteBot definition

--- stevio.py
+++ stevio.py
@@ -11,52 +11,52 @@ from selenium.common.exceptions import NoSuchElementException
 browser = webdriver.Chrome('/Users/stevemint/Downloads/chromedriver')
 
 
+def executeBot():
+    browser.get('https://shop.funko.com/collections/funko-shop-exclusives')
+    input_sq = "star wars"
+    input_mail = "tortugarara@gmail.com"
+    input_name = "Letinsky"
+    input_last = "Molinisky"
+    address = "lalaland 29"
+    apartment = "123"
+    city = "saltadilla"
+    zipcode = "89909"
 
-browser.get('https://shop.funko.com/collections/funko-shop-exclusives')
-input_sq = "star wars"
-input_mail = "leetymolina@gmail.com"
-input_name = "Letinsky"
-input_last = "Molinisky"
-address = "lalaland 29"
-apartment = "123"
-city = "saltadilla"
-zipcode = "10300"
 
 
+    browser.find_element_by_xpath('/html/body/div[1]/header/div[2]/div/div[1]/div[4]/div/label')
+    sqbutton = browser.find_element_by_xpath('/html/body/div[1]/header/div[2]/div/div[1]/div[4]/div/label')
+    sqbutton.get_attribute('href')
+    sqbutton.click()
 
-browser.find_element_by_xpath('/html/body/div[1]/header/div[2]/div/div[1]/div[4]/div/label')
-sqbutton = browser.find_element_by_xpath('/html/body/div[1]/header/div[2]/div/div[1]/div[4]/div/label')
-sqbutton.get_attribute('href')
-sqbutton.click()
+    browser.find_element_by_xpath('/html/body/div[1]/header/div[3]/div/form/div/div[1]/input')
+    sqtextfield = browser.find_element_by_xpath('/html/body/div[1]/header/div[3]/div/form/div/div[1]/input')
+    sqtextfield.send_keys(input_sq)
+    searchbutton = browser.find_element_by_xpath('/html/body/div[1]/header/div[3]/div/form/div/div[2]/button')
+    searchbutton.click()
 
-browser.find_element_by_xpath('/html/body/div[1]/header/div[3]/div/form/div/div[1]/input')
-sqtextfield = browser.find_element_by_xpath('/html/body/div[1]/header/div[3]/div/form/div/div[1]/input')
-sqtextfield.send_keys(input_sq)
-searchbutton = browser.find_element_by_xpath('/html/body/div[1]/header/div[3]/div/form/div/div[2]/button')
-searchbutton.click()
+    addtocartbutton = browser.find_element_by_xpath('/html/body/div[2]/main/div/div/div[1]/a/form/button/span')
+    addtocartbutton.click()
 
-addtocartbutton = browser.find_element_by_xpath('/html/body/div[2]/main/div/div/div[1]/a/form/button/span')
-addtocartbutton.click()
+    checkoutbutton = browser.find_element_by_xpath('/html/body/div[2]/main/div[1]/div/div/form/div/div/div[2]/div/button[2]')
+    checkoutbutton.click()
+    checkoutbutton.click()
 
-checkoutbutton = browser.find_element_by_xpath('/html/body/div[2]/main/div[1]/div/div/form/div/div/div[2]/div/button[2]')
-checkoutbutton.click()
-checkoutbutton.click()
+    namefield = browser.find_element_by_xpath('/html/body/div[2]/div/div[1]/div[2]/div/form/div[1]/div[2]/div[2]/div/div[1]/div/input')
+    namefield.send_keys(input_name)
 
-namefield = browser.find_element_by_xpath('/html/body/div[2]/div/div[1]/div[2]/div/form/div[1]/div[2]/div[2]/div/div[1]/div/input')
-namefield.send_keys(input_name)
+    lastfield = browser.find_element_by_xpath('/html/body/div[2]/div/div[1]/div[2]/div/form/div[1]/div[2]/div[2]/div/div[2]/div/input')
+    lastfield.send_keys(input_last)
 
-lastfield = browser.find_element_by_xpath('/html/body/div[2]/div/div[1]/div[2]/div/form/div[1]/div[2]/div[2]/div/div[2]/div/input')
-lastfield.send_keys(input_last)
+    addressfield = browser.find_element_by_xpath('/html/body/div[2]/div/div[1]/div[2]/div/form/div[1]/div[2]/div[2]/div/div[4]/div/input')
+    addressfield.send_keys(address)
 
-addressfield = browser.find_element_by_xpath('/html/body/div[2]/div/div[1]/div[2]/div/form/div[1]/div[2]/div[2]/div/div[4]/div/input')
-addressfield.send_keys(address)
+    apartmentfield = browser.find_element_by_xpath('/html/body/div[2]/div/div[1]/div[2]/div/form/div[1]/div[2]/div[2]/div/div[5]/div/input')
+    apartmentfield.send_keys(apartment)
 
-apartmentfield = browser.find_element_by_xpath('/html/body/div[2]/div/div[1]/div[2]/div/form/div[1]/div[2]/div[2]/div/div[5]/div/input')
-apartmentfield.send_keys(apartment)
+    cityfield = browser.find_element_by_xpath('/html/body/div[2]/div/div[1]/div[2]/div/form/div[1]/div[2]/div[2]/div/div[6]/div/input')
+    cityfield.send_keys(city)
 
-cityfield = browser.find_element_by_xpath('/html/body/div[2]/div/div[1]/div[2]/div/form/div[1]/div[2]/div[2]/div/div[6]/div/input')
-cityfield.send_keys(city)
-
-zipfield =  browser.find_element_by_xpath("/html/body/div[2]/div/div[1]/div[2]/div/form/div[1]/div[2]/div[2]/div/div[9]/div/input")
-zipfield.send_keys(zipcode)
-
+    zipfield =  browser.find_element_by_xpath("/html/body/div[2]/div/div[1]/div[2]/div/form/div[1]/div[2]/div[2]/div/div[9]/div/input")
+    zipfield.send_keys(zipcode)
+executeBot()
